09b2b9dda8af7d491688a615aad5e0a8ef00d35b
Marcelo Araújo <jeff@sti-openSUSE.site>
2012-05-16 20:40:06 -0300

    Folders

--- main.py
+++ main.py
@@ -1,20 +1,13 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import sys
-import puteiroManager, addClientForm
-from PyQt4 import QtCore, QtGui
-from PyQt4.Qt import *
+from app.Controllers.Clients_Controller import *
 
-if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    MainWindow = QtGui.QMainWindow()
-    ui = puteiroManager.Ui_MainWindow()
-    ui.setupUi(MainWindow)
-    MainWindow.show()
-    #sys.exit(app.exec_())
-    addClient = QtGui.QWidget()
-    ui = addClientForm.Ui_addClientForm()
-    ui.setupUi(addClient)
-    addClient.show()
-    sys.exit(app.exec_())
+def main():
+	c = Clients_Controller()
+	c.listAll()
+	#c.listOne(2)
+
+if __name__ == '__main__':
+	main()
